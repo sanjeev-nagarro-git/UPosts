ee3645676fe5d61e03af58f813a5c86dd08041ed
Sanjeev Mishra <sanjeev.mishra01@nagarro.com>
2024-08-29 20:05:25 +0530

    Initial Commit

---
+++ UPosts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UPosts
+//
+//  Created by Sanjeev Mishra on 29/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
